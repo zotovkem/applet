0eb68cb5828b96ab00747f5c8bd8d47005b25d2a
admin-ПК\admin <zotov_86@mail.ru>
2017-05-03 16:34:16 +0700

    решил задачу 27 левел 18

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -5,24 +5,55 @@ package com.javarush.task.task18.task1827;
 */
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         if (args[0].equals("-c")) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = reader.readLine();
+            reader.close();
+
+            ArrayList<String> contentFile = readToList(fileName);
+
             String productName = "";
             for (int i = 1; i < args.length - 2; i++) {
                 productName += args[i] + " ";
             }
             String productPrice = args[args.length - 2];
             String productQuality = args[args.length - 1];
-            String fileName = reader.readLine();
-            String productId = String.valueOf(findMaxId(fileName));
-            reader.close();
+            String productId = String.valueOf(findMaxId(contentFile));
+            contentFile.add(spaseFilling(productId, 8) + spaseFilling(productName, 30) + spaseFilling(productPrice, 8) + spaseFilling(productQuality, 4));
 
-            PrintWriter outputStream = new PrintWriter(new FileWriter(fileName, true));
-            outputStream.println(spaseFilling(productId, 8) + spaseFilling(productName, 30) + spaseFilling(productPrice, 8) + spaseFilling(productQuality, 4));
-            outputStream.close();
+            listToFile(contentFile,fileName);
+        }
+    }
+
+    private static ArrayList<String> readToList(String fileName){
+        ArrayList<String> contentFile = new ArrayList<>();
+        BufferedReader inputStream = null;
+        try {
+            inputStream = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+            while (inputStream.ready()) {
+                contentFile.add(inputStream.readLine());
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contentFile;
+    }
+
+    private static void listToFile(ArrayList<String> contentFile, String fileName) {
+        PrintWriter outputStream = null;
+        try {
+            outputStream = new PrintWriter(new FileWriter(fileName));
+        for (String str :contentFile) {
+            outputStream.println(str);
+        }
+        outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -39,21 +70,20 @@ public class Solution {
         return result.toString();
     }
 
-    private static int findMaxId(String fileName) throws Exception {
-        BufferedReader inputStream = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
-        int max = 0;
+    private static int findMaxId(ArrayList<String> contentFile) {
+        int max;
+        String deb = contentFile.get(0).substring(0, 8);
+        if (contentFile.size()>0) {
+            max = Integer.parseInt(contentFile.get(0).substring(0, 8).trim());
+        }else{
+            max = 0;
+        }
         Integer id = 0;
-        while (inputStream.ready()) {
-            try {
-                String str = inputStream.readLine().substring(0, 8).trim();
-                id = Integer.parseInt(str);
-            } catch (Exception e) {
-                continue;
-            }
+        for (String str :contentFile) {
+            id = Integer.parseInt(str.substring(0, 8).trim());
             if (id > max)
                 max = id;
         }
-        inputStream.close();
         return ++max;
     }
 }
