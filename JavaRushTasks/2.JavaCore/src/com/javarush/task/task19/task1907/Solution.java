629f81b6e0c8a7479afe70401d37443c3260fb08
Evgeniy <zotov_86@mail.ru>
2017-05-10 00:10:58 +0700

    решил задачу 8 левел 19

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -8,8 +8,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
