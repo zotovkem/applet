f6206802a33ee3e630af6ad52ce02bc7084ebeae
admin-ПК\admin <zotov_86@mail.ru>
2017-06-21 13:09:31 +0700

    решил задачу 15 левел 19

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        testString.printSomething();
+        System.setOut(consoleStream);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(reader.readLine());
+            fileOutputStream.write(outputStream.toByteArray());
+            fileOutputStream.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(outputStream.toString());
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
