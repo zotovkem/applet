74b7aec06558024e0ed3fe4e9e61a08b9c37103e
admin-ПК\admin <zotov_86@mail.ru>
2017-08-07 17:00:50 +0700

    решил 21 задачу уровень 19

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -6,47 +6,46 @@ package com.javarush.task.task19.task1920;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) {
+        HashMap<String, Double> amountList = new HashMap<>();
+        double maxAmount = 0;
+        StringBuilder allStr = new StringBuilder();
+
         try {
             FileReader fileReader = new FileReader(args[0]);
-            HashMap<String, Double> amountList = new HashMap<>();
-            double maxAmount = 0;
-            StringBuilder allStr = new StringBuilder();
             while (fileReader.ready()) {
                 char[] arrBuf = new char[256];
                 fileReader.read(arrBuf);
                 allStr.append(arrBuf);
             }
             fileReader.close();
-            for (String lineStr :
-                    allStr.toString().split(System.lineSeparator())) {
-                String[] wordStr = lineStr.split(" ");
-                String secondName = wordStr[0].trim().replaceAll("\\s","");
-                double amount = Double.parseDouble(wordStr[1].trim());
-                if (amountList.containsKey(secondName)) {
-                    amount = amount + amountList.get(secondName);
-                    amountList.replace(secondName, amount);
-                }
-                else
-                    amountList.put(secondName, amount);
-
-                if (amount > maxAmount)
-                    maxAmount = amount;
-            }
-            for (Map.Entry<String, Double> entry :
-                    amountList.entrySet()) {
-                if (entry.getValue() == maxAmount)
-                    System.out.println(entry.getKey());
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for (String lineStr :
+                allStr.toString().split(System.lineSeparator())) {
+            String[] wordStr = lineStr.split(" ");
+            String secondName = wordStr[0].trim().replaceAll("\\s", "");
+            double amount = Double.parseDouble(wordStr[1].trim());
+            if (amountList.containsKey(secondName)) {
+                amount = amount + amountList.get(secondName);
+                amountList.replace(secondName, amount);
+            } else
+                amountList.put(secondName, amount);
+
+            if (amount > maxAmount)
+                maxAmount = amount;
+        }
+        for (Map.Entry<String, Double> entry :
+                amountList.entrySet()) {
+            if (entry.getValue() == maxAmount)
+                System.out.println(entry.getKey());
+        }
+
 
     }
 }
--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -1,7 +1,12 @@
 package com.javarush.task.task19.task1921;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Хуан Хуанович
@@ -11,6 +16,36 @@ public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
     public static void main(String[] args) {
+        HashMap<String, Double> amountList = new HashMap<>();
+        double maxAmount = 0;
+        StringBuilder allStr = new StringBuilder();
+        if (args.length > 0) {
+            try {
+                FileReader fileReader = new FileReader(args[0]);
+                while (fileReader.ready()) {
+                    char[] arrBuf = new char[256];
+                    fileReader.read(arrBuf);
+                    allStr.append(arrBuf);
+                }
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (String lineStr :
+                    allStr.toString().split(System.lineSeparator())) {
+                String nameStr = lineStr.trim().replaceAll("\\d", "").trim();
+                String dateStr = lineStr.trim().replaceAll(nameStr, "").trim().replaceAll(" ", ".");
+                SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+                Date birthDay = null;
 
+                try {
+                    birthDay = format.parse(dateStr);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                PEOPLE.add(new Person(nameStr,birthDay));
+
+            }
+        }
     }
 }
