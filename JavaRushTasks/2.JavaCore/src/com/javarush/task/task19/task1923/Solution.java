832c8ffe050f136c2e88c983413e4cdf9aac9595
admin-ПК\admin <zotov_86@mail.ru>
2017-08-11 16:32:17 +0700

    решил 23 задачу уровень 19

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -4,7 +4,40 @@ package com.javarush.task.task19.task1923;
 Слова с цифрами
 */
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
     public static void main(String[] args) {
+        StringBuilder allStr = new StringBuilder();
+        if (args.length > 1) {
+            try {
+                FileReader fileReader = new FileReader(args[0]);
+
+                while (fileReader.ready()) {
+                    char[] arrBuf = new char[256];
+                    fileReader.read(arrBuf);
+                    allStr.append(arrBuf);
+                }
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String regEx = "(\\S+\\d+\\S+)|(\\S+\\d)|(\\d+\\S+)|(\\d)";
+            Pattern p = Pattern.compile(regEx);
+            Matcher m = p.matcher(allStr);
+            try {
+                FileWriter fileWriter = new FileWriter(args[1]);
+                while (m.find()) {
+                    fileWriter.write(allStr.substring(m.start(), m.end())+" ");
+                }
+                fileWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
