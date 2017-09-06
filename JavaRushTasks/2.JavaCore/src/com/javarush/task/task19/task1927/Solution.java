2f3cb578b90ce522f4388b4c1bd0b3d8bfecf375
admin-ПК\admin <zotov_86@mail.ru>
2017-09-06 10:12:12 +0700

    решил задачу 27-28 левел 19

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task19.task1927;
+
+/* 
+Контекстная реклама
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String result = outputStream.toString();
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        int count = 1;
+        for (String str :
+                result.split(System.lineSeparator())) {
+            System.out.println(str);
+            if (count % 2 == 0) {
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+            count++;
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
