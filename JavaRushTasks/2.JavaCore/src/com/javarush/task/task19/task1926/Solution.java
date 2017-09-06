d0d87b64b8c651bdf886eecbf6a465c4becc44a2
admin-ПК\admin <zotov_86@mail.ru>
2017-09-06 08:28:09 +0700

    решил задачу 26 левел 19

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1926/Solution.java
@@ -4,7 +4,33 @@ package com.javarush.task.task19.task1926;
 Перевертыши
 */
 
+import java.io.*;
+import java.nio.Buffer;
+
 public class Solution {
     public static void main(String[] args) {
-    }
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String readFile = reader.readLine();
+                reader.close();
+
+                BufferedReader fileReader = new BufferedReader(new FileReader(readFile));
+
+                StringBuilder allStr = new StringBuilder();
+                while (fileReader.ready()) {
+                    allStr.append(new StringBuilder(fileReader.readLine()).reverse());
+                    allStr.append(System.lineSeparator());
+                }
+
+                System.out.println(allStr);
+
+                fileReader.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
 }
