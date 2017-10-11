b0120c8c38c95d0cb6b49a37371633f3b447453b
testkomp\admin <zotov_86@mail.ru>
2017-10-11 17:50:39 +0700

    промежуточный коммит 25 задачи

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -1,72 +1,93 @@
 package com.javarush.task.task20.task2025;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-/*
-Алгоритмы-числа
-*/
 public class Solution {
-    public static int getExtent(long N) {
-        int extent = 0;
-        do {
-            extent++;
-        } while ((N = N / 10) != 0);
-        return extent;
+    public static void main(String[] args)
+    {
+        long memoryStart = Runtime.getRuntime().freeMemory();
+        long startTime = System.currentTimeMillis();
+
+        System.out.println(Arrays.toString(getNumbers(912985154)));
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        long memoryEnd = Runtime.getRuntime().freeMemory();
+        long estimatedMemory = memoryStart - memoryEnd;
+        System.out.println("Время подсчета: " + (double) estimatedTime / 1000 + " c");
+        System.out.println("Использованная память: " + (double)estimatedMemory / 1024 / 1024 + " МБ");
     }
 
-    public static int[] getArr(long N) {
-        int extent = getExtent(N);
-        int[] numbers = new int[extent];
-        int j = 0;
-        while (N != 0) {
-            numbers[j++] = (int) N % 10;
-            N = N / 10;
-        }
-        return numbers;
-    }
 
-    public static long calc(int[] numbers) {
-        long result = 0;
-        for (int i = 0; i < numbers.length - 1; i++) {
-            result += Math.pow(numbers[i], numbers.length);
-        }
-        return result;
-    }
-
-    public static long[] getNumbers(long N) {
-        long[] result = null;
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < N; i++)
-//        int i = 370;
+    public static int[] getNumbers(long N) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        long[][] pws = new long[10][11];//таблица степеней
+        for (int i = 0; i < 10; i++)
         {
-            if (calc(getArr(i)) == i) {
-                res.add(i);
+            long pw = 1;
+            for (int j = 1; j < 11; j++)
+            {
+                pw = pw * i;
+                pws[i][j] = pw;
             }
         }
-        result = new long[res.size()];
-        int j = 0;
-        for (Integer numb :
-                res) {
-            result[j++] = numb;
+
+        int[] symbols = new int[10];
+        for (int i = 0; i < N; i++)
+        {
+            int value = i;
+            int lastSym = -1;
+            int length = 0;
+            boolean uselessNumber = false;
+            while (value != 0)//разбираю число на цифры
+            {
+                int sym = value % 10;
+                if (sym < sym % 10)//отсекаю ненужные варианты
+                {
+                    uselessNumber = true;
+                    break;
+                }
+                lastSym = sym;
+                symbols[length] = sym;
+                value = value / 10;
+                length++;
+            }
+
+            if (uselessNumber){continue;}
+
+            int res = 0;//вычисляю сумму степеней цифр
+            for (int num = 0; num < length; num++)
+            {
+                res += pws[symbols[num]][length];
+            }
+            if (res >= N || res < 0)//отсекаю все суммы не укладывающиеся в диапазон
+            {
+                continue;
+            }
+            value = res;
+            length = 0;
+            while (value != 0)//разбираю полученную сумму степеней цифр на цифры
+            {
+                symbols[length] = value % 10;
+                value = value / 10;
+                length++;
+            }
+            int res2 = 0;
+            for (int num = 0; num < length; num++)//опять вычисляю сумму степененй цифр
+            {
+                res2 += pws[symbols[num]][length];
+            }
+            if (res == res2 && (!list.contains(res)))//сравниваю две суммы степеней цифр и, если они равны, то это нужное число
+            {
+                list.add(res);
+            }
         }
-//        result = res.toArray();
-        return result;
-    }
-
-
-    public static void main(String[] args) {
-        long a1=System.currentTimeMillis();
-        long a2=Runtime.getRuntime().freeMemory();
-        long[]a3=getNumbers(Long.MAX_VALUE);
-        long a4=Runtime.getRuntime().freeMemory();
-        long a5=System.currentTimeMillis();
-        long a6=a2-a4;
-        long a7=a5-a1;
-        System.out.println(a2/(1024*1024));
-        System.out.println(a4/(1024*1024));
-        System.out.println(a6/(1024*1024));
-        System.out.println((double)a7/1000);
-
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)//перекладываю найденные числа Армстронга в массив
+        {
+            ret[i] = list.get(i);
+        }
+        Arrays.sort(ret);//сортирую массив
+        return ret;
     }
 }
