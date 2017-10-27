81da391aa3ba525ee13f8231cc3c6414c8d3f353
testkomp\admin <zotov_86@mail.ru>
2017-10-27 17:00:40 +0700

    промежуточный коммит 11 задача 30 уровень

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task31.task3111;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+
+public class SearchFileVisitor extends SimpleFileVisitor<Path> {
+
+    private List<Path> foundFiles;
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        byte[] content = Files.readAllBytes(file); // размер файла: content.length
+
+        return super.visitFile(file, attrs);
+    }
+
+    public void setPartOfName(String partOfName) {
+        this.partOfName = partOfName;
+    }
+
+    public void setPartOfContent(String partOfContent) {
+        this.partOfContent = partOfContent;
+    }
+
+    public void setMinSize(int minSize) {
+        this.minSize = minSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public List<Path> getFoundFiles() {
+        return foundFiles;
+    }
+}
