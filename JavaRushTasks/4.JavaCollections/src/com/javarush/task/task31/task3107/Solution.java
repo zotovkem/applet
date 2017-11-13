9b6b27d5b20139cb0b4bfa3737cfe23b9bb00d31
testkomp\admin <zotov_86@mail.ru>
2017-11-13 14:56:12 +0700

    решил задача 7 и 8  31 уровень

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3107/Solution.java
@@ -1,12 +1,27 @@
 package com.javarush.task.task31.task3107;
 
-/* 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/*
 Null Object Pattern
 */
 public class Solution {
     private FileData fileData;
 
-    public Solution(String pathToFile) {
+    public Solution(String pathToFile){
+        Path fileName = null;
+        try {
+            fileName = Paths.get(pathToFile);
+            fileData = new ConcreteFileData(Files.isHidden(fileName),
+                    Files.isExecutable(fileName),
+                    Files.isDirectory(fileName),
+                    Files.isWritable(fileName));
+        } catch (Exception e) {
+            fileData = new NullFileData(e);
+        }
     }
 
     public FileData getFileData() {
